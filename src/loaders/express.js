29e181a72c56e2eac0abeca0c8adebfec2f5d161
samrmay <samrmay@sas.upenn.edu>
2021-01-09 12:33:05 -0500

    added cors header

--- src/loaders/express.js
+++ src/loaders/express.js
@@ -1,7 +1,14 @@
 import express from "express";
+import cors from "cors";
 import routes from "../api";
 
 export default (app) => {
+  app.use(
+    cors({
+      origin: process.env.CLIENT_URL,
+    })
+  );
+
   app.use(express.json());
   app.use("/api", routes());
 };
