fcd9103ecc8472a489794625bfc1ae4ce8835d0c
samrmay <samrmay@sas.upenn.edu>
2021-01-08 23:57:07 -0500

    add mongoose loader

--- src/loaders/index.js
+++ src/loaders/index.js
@@ -1,7 +1,14 @@
 import loadExpress from "./express.js";
+import loadMongoose from "./mongoose.js";
 
 export default async (app) => {
+  require("dotenv").config();
+
   console.log("Loading express");
   loadExpress(app);
   console.log("Express loaded");
+
+  console.log("Loading mongoose");
+  loadMongoose(app);
+  console.log("Mongoose Loaded");
 };
