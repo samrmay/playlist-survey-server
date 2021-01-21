15eab2cf4554ea973e726b0e5a8f540f29c95164
samrmay <samrmay@sas.upenn.edu>
2021-01-21 13:16:20 -0500

    update survey model

--- src/models/survey.model.js
+++ src/models/survey.model.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const surveySchema = new mongoose.Schema({
-  name: String,
-  // Owner is spotify user id
-  owner: String,
-  playlistSpotifyId: { type: String, required: true },
-  trackRankings: [{ trackSpotifyId: String, trackRanking: Number }],
-  refreshToken: { type: String, required: true },
-});
+const surveySchema = new mongoose.Schema(
+  {
+    name: String,
+    // Owner is spotify user id
+    owner: String,
+    playlistSpotifyId: { type: String, required: true },
+    trackRankings: [{ trackSpotifyId: String, trackRanking: Number }],
+    refreshToken: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 export const Survey = mongoose.model("Survey", surveySchema);
